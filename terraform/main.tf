968ae73f829b6625e0ffcba9fd576ac5bdbdcb61
clincha <angus.clinch@gmail.com>
2022-05-26 13:16:11 +0100

    Let's hope I didn't expose any keys this time

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,8 +10,15 @@ provider "proxmox" {
   pm_api_url = "https://192.168.2.174:8006/api2/json"
 }
 
-resource "proxmox_vm_qemu" "rhel8-instance" {
-  name        = "kube-worker-0"
+resource "proxmox_vm_qemu" "rhel8-worker" {
+  for_each = {
+    edi-kubeworker-1 = "1"
+    edi-kubeworker-2 = "2"
+    edi-kubeworker-3 = "3"
+    edi-runner-1     = "4"
+  }
+
+  name        = each.key
   target_node = "edi-s-01"
   clone       = "template-rhel8"
   full_clone  = false
@@ -22,18 +29,20 @@ resource "proxmox_vm_qemu" "rhel8-instance" {
   memory  = 4096
 
   os_type   = "cloud-init"
-  ipconfig0 = "ip=192.168.2.160/24,gw=192.168.2.1"
+  ipconfig0 = "ip=192.168.2.16${each.value}/24,gw=192.168.2.1"
+
+  tags = "base,kubernetes-worker"
 
   provisioner "remote-exec" {
     connection {
-      host        = "192.168.2.160"
+      host        = "192.168.2.16${each.value}"
       type        = "ssh"
       user        = "ansible"
       private_key = file("./ansible")
       port        = 22
     }
 
-    inline = ["sudo hostnamectl set-hostname worker1"]
+    inline = ["sudo hostnamectl set-hostname ${each.key}"]
   }
 
 }
