28ecb83b79dfb47e0f3824e31ed3c46d976a0dd4
clincha <angus.clinch@gmail.com>
2022-06-08 18:39:47 +0100

    ./

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ provider "proxmox" {
 # Github runners
 module "edi-runner-1" {
   name      = "edi-runner-01"
-  source    = "modules/rhel8"
+  source    = "./modules/rhel8"
   tags      = "base,github_runner"
   ip_subnet = "192.168.2.164/24"
   port      = "16004"
@@ -25,21 +25,21 @@ module "edi-runner-1" {
 # Kubernetes workers
 module "edi-kubeworker-3" {
   name      = "edi-kubeworker-3"
-  source    = "modules/rhel8"
+  source    = "./modules/rhel8"
   tags      = "base,kubernetes_worker"
   ip_subnet = "192.168.2.163/24"
   port      = "16003"
 }
 module "edi-kubeworker-2" {
   name      = "edi-kubeworker-2"
-  source    = "modules/rhel8"
+  source    = "./modules/rhel8"
   tags      = "base,kubernetes_worker"
   ip_subnet = "192.168.2.162/24"
   port      = "16002"
 }
 module "edi-kubeworker-1" {
   name      = "edi-kubeworker-1"
-  source    = "modules/rhel8"
+  source    = "./modules/rhel8"
   tags      = "base,kubernetes_worker"
   ip_subnet = "192.168.2.161/24"
   port      = "16001"
