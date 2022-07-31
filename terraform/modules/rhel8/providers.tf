1dd2ca3184e7d230b5f64badbc0fdb96a02c8c8d
clincha <angus.clinch@gmail.com>
2022-07-31 01:05:28 +0100

    proxmox

--- terraform/modules/rhel8/providers.tf
+++ terraform/modules/rhel8/providers.tf
@@ -1,3 +1,11 @@
-provider "telmate/proxmox" {
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+    }
+  }
+}
+
+provider "proxmox" {
   pm_api_url = "https://clincha.co.uk:8006/api2/json"
 }
