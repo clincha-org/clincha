23246d8c5a5152421f4cf4369c1f225f600f2c9b
clincha <angus.clinch@gmail.com>
2022-07-31 10:56:07 +0100

    providers

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    proxmox = {
-      source = "Telmate/proxmox"
-    }
-  }
-}
-
-provider "proxmox" {
-  pm_api_url          = "https://clincha.co.uk:8006/api2/json"
-  pm_api_token_id     = "terraform@pam!terraform"
-  pm_api_token_secret = var.proxmox_token_secret
-  pm_tls_insecure     = true
-}
-
 # Github runners
 module "edi-runner-1" {
   name      = "edi-runner-01"
---
+++ terraform/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "telmate/proxmox"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url          = "https://clincha.co.uk:8006/api2/json"
+  pm_api_token_id     = "terraform@pam!terraform"
+  pm_api_token_secret = var.proxmox_token_secret
+  pm_tls_insecure     = true
+}
