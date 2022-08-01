fca4b8a53d458c14111b627850b81b3f530edf15
clincha <angus.clinch@gmail.com>
2022-08-01 23:00:18 +0100

    datatypes

--- terraform/modules/rhel8/variables.tf
+++ terraform/modules/rhel8/variables.tf
@@ -2,58 +2,67 @@
 variable "ansible_id_rsa" {
   default   = ""
   sensitive = true
-  type = string
+  type      = string
 }
 
 variable "hostname" {
   default = ""
-  type = string
+  type    = string
 }
 
 variable "name" {
   default = ""
-  type = string
+  type    = string
 }
 
 variable "port" {
   default = ""
-  type = number
+  type    = number
 }
 
 # Optional
 variable "target_node" {
   default = "edi-s-01"
+  type    = string
 }
 
 variable "source_vm" {
   default = "template-rhel8"
+  type    = string
 }
 
 variable "full_clone" {
   default = true
+  type    = bool
 }
 
 variable "agent" {
   default = 1
+  type    = number
 }
 
 variable "sockets" {
   default = 2
+  type    = number
 }
 
 variable "cores" {
   default = 2
+  type    = number
 }
 
 variable "memory" {
   default = 4096
+  type    = number
 }
 
 variable "ip_subnet" {
   # e.g. 192.168.1.2/24
   default = ""
+  type    = string
 }
 
 variable "tags" {
-  default = "base"
+  default = ["base"]
+  type    = list(string)
 }
--- terraform/varibales.tf
+++ terraform/varibales.tf
@@ -1,9 +1,11 @@
 variable "ansible_id_rsa" {
   default   = ""
   sensitive = true
+  type = string
 }
 
 variable "proxmox_token_secret" {
   default   = ""
   sensitive = true
+  type = string
 }
