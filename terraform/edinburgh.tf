a17bdceeab74ec496ccfd7a504a5c3f3d2a3a34b
clincha <angus.clinch@gmail.com>
2022-09-28 22:45:00 +0100

    ip

--- terraform/edinburgh.tf
+++ terraform/edinburgh.tf
@@ -3,7 +3,7 @@ module "edi-kubeworker-1" {
   name           = "edi-kubeworker-1"
   source         = "./modules/rhel8"
   tags           = ["base", "kubernetes_worker"]
-  ip_subnet      = "192.168.2.161"
+  ip             = "192.168.2.161"
   subnet_mask    = "24"
   gateway        = "192.168.2.1"
   ansible_id_rsa = var.ansible_id_rsa
@@ -15,7 +15,7 @@ module "edi-kubeworker-2" {
   name           = "edi-kubeworker-2"
   source         = "./modules/rhel8"
   tags           = ["base", "kubernetes_worker"]
-  ip_subnet      = "192.168.2.162"
+  ip             = "192.168.2.162"
   subnet_mask    = "24"
   gateway        = "192.168.2.1"
   ansible_id_rsa = var.ansible_id_rsa
@@ -27,7 +27,7 @@ module "edi-kubeworker-3" {
   name           = "edi-kubeworker-3"
   source         = "./modules/rhel8"
   tags           = ["base", "kubernetes_worker"]
-  ip_subnet      = "192.168.2.163"
+  ip             = "192.168.2.163"
   subnet_mask    = "24"
   gateway        = "192.168.2.1"
   ansible_id_rsa = var.ansible_id_rsa
@@ -41,7 +41,7 @@ module "edi-runner-1" {
   name           = "edi-runner-01"
   source         = "./modules/rhel8"
   tags           = ["base", "github_runner"]
-  ip_subnet      = "192.168.2.164"
+  ip             = "192.168.2.164"
   subnet_mask    = "24"
   gateway        = "192.168.2.1"
   ansible_id_rsa = var.ansible_id_rsa
