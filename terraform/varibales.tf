adfb6f9f9e800af8a9a36c47be28ae9b8570bf4e
clincha <angus.clinch@gmail.com>
2022-10-09 11:43:52 +0100

    ansible PK

--- terraform/varibales.tf
+++ terraform/varibales.tf
@@ -1,17 +1,14 @@
 variable "ansible_id_rsa" {
-  default   = ""
   sensitive = true
   type = string
 }
 
 variable "edinburgh_proxmox_token_secret" {
-  default   = ""
   sensitive = true
   type = string
 }
 
 variable "bristol_proxmox_token_secret" {
-  default = ""
   sensitive = true
   type = string
 }
