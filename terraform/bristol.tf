e95190e4dc73a96ea36d3a807fecedaf555f4667
clincha <angus.clinch@gmail.com>
2022-10-14 21:48:15 +0100

    master nodes

--- terraform/bristol.tf
+++ terraform/bristol.tf
@@ -1,4 +1,19 @@
 # Kubernetes workers
+module "bri-master-1" {
+  name           = "bri-master-1"
+  source         = "./modules/rhel8"
+  tags           = ["base", "kubernetes_worker", "kubernetes_master"]
+  ip             = "192.168.1.20"
+  subnet_mask    = "24"
+  gateway        = "192.168.1.1"
+  ansible_id_rsa = var.ansible_id_rsa
+  providers      = {
+    proxmox = proxmox.bri-s-01
+  }
+  target_node = "bri-s-01"
+  source_vm   = "bri-s-01-template"
+  desc        = "Kubernetes master node in Bristol (region 1)"
+}
 module "bri-kubeworker-1" {
   name           = "bri-kubeworker-1"
   source         = "./modules/rhel8"
