81aa36826b04d205e6c808df7cd90aa6dd07e544
clincha <angus.clinch@gmail.com>
2023-05-04 22:17:15 +0100

    removal

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -7,14 +7,6 @@ terraform {
 }
 
 provider "proxmox" {
-  alias               = "edi-s-01"
-  pm_api_url          = "https://192.168.2.174:8006/api2/json"
-  pm_api_token_id     = "terraform@pam!terraform"
-  pm_api_token_secret = var.edinburgh_proxmox_token_secret
-  pm_tls_insecure     = true
-}
-
-provider "proxmox" {
   alias               = "bri-s-01"
   pm_api_url          = "https://192.168.1.11:8006/api2/json"
   pm_api_token_id     = "terraform@pam!terraform"
@@ -25,6 +17,14 @@ provider "proxmox" {
 provider "proxmox" {
   alias               = "bri-s-02"
   pm_api_url          = "https://192.168.1.12:8006/api2/json"
+  pm_api_token_id     = "terraform@pam!terraform"
+  pm_api_token_secret = var.bristol_proxmox_token_secret
+  pm_tls_insecure     = true
+}
+
+provider "proxmox" {
+  alias               = "bri-s-03"
+  pm_api_url          = "https://192.168.1.13:8006/api2/json"
   pm_api_token_id     = "terraform@pam!terraform"
   pm_api_token_secret = var.bristol_proxmox_token_secret
   pm_tls_insecure     = true
