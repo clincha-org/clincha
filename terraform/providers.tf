a8423c4eff84e3ec737f04b364cad52ab8425ffa
clincha <angus.clinch@gmail.com>
2023-05-05 20:00:06 +0100

    version and packer

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,7 +1,8 @@
 terraform {
   required_providers {
     proxmox = {
-      source = "telmate/proxmox"
+      source = "Telmate/proxmox"
+      version = "2.9.14"
     }
   }
 }
@@ -9,23 +10,23 @@ terraform {
 provider "proxmox" {
   alias               = "bri-s-01"
   pm_api_url          = "https://192.168.1.11:8006/api2/json"
-  pm_api_token_id     = "terraform@pam!terraform"
+  pm_api_token_id     = "packer@pam!packer"
   pm_api_token_secret = var.bristol_proxmox_token_secret
   pm_tls_insecure     = true
 }
-
-provider "proxmox" {
-  alias               = "bri-s-02"
-  pm_api_url          = "https://192.168.1.12:8006/api2/json"
-  pm_api_token_id     = "terraform@pam!terraform"
-  pm_api_token_secret = var.bristol_proxmox_token_secret
-  pm_tls_insecure     = true
-}
-
-provider "proxmox" {
-  alias               = "bri-s-03"
-  pm_api_url          = "https://192.168.1.13:8006/api2/json"
-  pm_api_token_id     = "terraform@pam!terraform"
-  pm_api_token_secret = var.bristol_proxmox_token_secret
-  pm_tls_insecure     = true
-}
+#
+#provider "proxmox" {
+#  alias               = "bri-s-02"
+#  pm_api_url          = "https://192.168.1.12:8006/api2/json"
+#  pm_api_token_id     = "terraform@pam!terraform"
+#  pm_api_token_secret = var.bristol_proxmox_token_secret
+#  pm_tls_insecure     = true
+#}
+#
+#provider "proxmox" {
+#  alias               = "bri-s-03"
+#  pm_api_url          = "https://192.168.1.13:8006/api2/json"
+#  pm_api_token_id     = "terraform@pam!terraform"
+#  pm_api_token_secret = var.bristol_proxmox_token_secret
+#  pm_tls_insecure     = true
+#}
