866fffc0c802443910d63d8fd98c2152f88bf8c0
clincha <angus.clinch@gmail.com>
2023-06-03 17:43:59 +0100

    ARM_ACCESS_KEY

--- terraform/state.tf
+++ terraform/state.tf
@@ -1,9 +1,24 @@
 terraform {
-  cloud {
-    organization = "clinch-home"
-
-    workspaces {
-      name = "clincha"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstateclincha"
+    container_name       = "tfstateclincha"
+    key                  = "clincha_terraform.tfstate"
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg-tfstate" {
+  name     = "tfstate"
+  location = "eu-west-2"
 }
