a5702aec5bdf0e3f8145ab20892aa22afd9a5012
clincha <angus.clinch@gmail.com>
2023-06-08 21:56:43 +0100

    rhel8

--- terraform/bristol.tf
+++ terraform/bristol.tf
@@ -1,7 +1,7 @@
 # Kubernetes workers
 module "bri-master-1" {
   name           = "bri-master-1"
-  source         = "modules/rocky8"
+  source         = "modules/rhel8"
   tags           = ["base", "kubernetes_worker", "kubernetes_master"]
   ip             = "192.168.1.20"
   gateway        = "192.168.1.1"
@@ -15,7 +15,7 @@ module "bri-master-1" {
 
 module "bri-kubeworker-1" {
   name           = "bri-kubeworker-1"
-  source         = "modules/rocky8"
+  source         = "modules/rhel8"
   tags           = ["base", "kubernetes_worker"]
   ip             = "192.168.1.21"
   gateway        = "192.168.1.1"
@@ -29,7 +29,7 @@ module "bri-kubeworker-1" {
 
 module "bri-kubeworker-2" {
   name           = "bri-kubeworker-2"
-  source         = "modules/rocky8"
+  source         = "modules/rhel8"
   tags           = ["base", "kubernetes_worker"]
   ip             = "192.168.1.22"
   gateway        = "192.168.1.1"
@@ -43,7 +43,7 @@ module "bri-kubeworker-2" {
 
 module "bri-kubeworker-3" {
   name           = "bri-kubeworker-3"
-  source         = "modules/rocky8"
+  source         = "modules/rhel8"
   tags           = ["base", "kubernetes_worker"]
   ip             = "192.168.1.23"
   gateway        = "192.168.1.1"
