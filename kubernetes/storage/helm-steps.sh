43eb531aff5e06db47527cf856da6131895b6b0f
clincha <angus.clinch@gmail.com>
2023-06-14 19:26:03 +0100

    flannel chart install

---
+++ kubernetes/storage/helm-steps.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+helm repo add ceph-csi https://ceph.github.io/csi-charts
+
+git clone https://github.com/ceph/ceph-csi.git
+
+cd ceph-csi/charts/ || exit
+
+kubectl create namespace "ceph-csi-rbd"
+
+helm install --namespace "ceph-csi-rbd" "ceph-csi-rbd" ceph-csi/ceph-csi-rbd
+
+helm status --namespace "ceph-csi-rbd" "ceph-csi-rbd"
+
+k get all -n ceph-csi-rbd
