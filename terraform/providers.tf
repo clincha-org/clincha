69531c1758966dfcf1e8a236fabef88aa8d76cbc
clincha <angus.clinch@gmail.com>
2024-01-11 21:02:47 +0000

    2.9.11

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     proxmox = {
       source  = "Telmate/proxmox"
-      version = "2.9.13"
+      version = "2.9.11"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
