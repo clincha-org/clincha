4fc6b85999e6f0039f281b3414c9eeaac152c946
clincha <angus.clinch@gmail.com>
2024-01-11 21:14:12 +0000

    tfstateclincha

--- terraform/state.tf
+++ terraform/state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate"
-    storage_account_name = "clincha"
+    storage_account_name = "tfstateclincha"
     container_name       = "tfstate"
     key                  = "clincha.tfstate"
   }
