ef7956b77618b0a490bf7d84156b02267f8aee98
clincha <angus.clinch@gmail.com>
2024-01-12 22:15:10 +0000

    run against my fork

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     proxmox = {
-      source  = "Telmate/proxmox"
-      version = "2.9.14"
+      source  = "github.com/clincha/terrafrom-provider-proxmox"
+      branch  = "master"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
