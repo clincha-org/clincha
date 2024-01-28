abafb25129347478e7ee7195a7ff4ca2bae2b17e
clincha <angus.clinch@gmail.com>
2024-01-28 16:09:50 +0000

    bump version inside as well

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     proxmox = {
       source  = "telmate/proxmox"
-      version = "1.0.12"
+      version = "1.0.14"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
