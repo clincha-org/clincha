e085497df6901ccfc916c7846ed925f401dafb0c
clincha <angus.clinch@gmail.com>
2024-01-28 20:22:18 +0000

    v1.0.16

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     proxmox = {
       source  = "telmate/proxmox"
-      version = "1.0.14"
+      version = "1.0.16"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
