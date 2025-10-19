12c0dc818759cac42d629baf46e99fc7e0cf8c53
clincha <angus.clinch@gmail.com>
2025-10-19 12:01:34 +0100

    kustomization for metallb

--- terraform/london/main.tf
+++ terraform/london/main.tf
@@ -5,7 +5,7 @@ module "k8s-lon-1" {
   vmid           = 111
   ip_address     = "10.2.0.101"
   gateway        = "10.2.0.1"
-  cores          = 2
+  cores          = 4
   memory         = 4096
   boot_disk_size = "50"
   network_bridge = "vmbr0"
@@ -18,7 +18,7 @@ module "k8s-lon-2" {
   vmid           = 112
   ip_address     = "10.2.0.102"
   gateway        = "10.2.0.1"
-  cores          = 2
+  cores          = 4
   memory         = 4096
   boot_disk_size = "50"
   network_bridge = "vmbr0"
@@ -31,7 +31,7 @@ module "k8s-lon-3" {
   vmid           = 113
   ip_address     = "10.2.0.103"
   gateway        = "10.2.0.1"
-  cores          = 2
+  cores          = 4
   memory         = 4096
   boot_disk_size = "50"
   network_bridge = "vmbr0"
