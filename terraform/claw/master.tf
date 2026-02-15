a7b8dd6139c4321da573ab3681370d1b8ef18f85
clincha <angus.clinch@gmail.com>
2026-02-15 06:37:31 +0000

    openclaw server

---
+++ terraform/claw/master.tf
@@ -0,0 +1,54 @@
+resource "proxmox_vm_qemu" "k8s-hawk-1" {
+  name        = var.name
+  target_node = var.target_node
+  vmid        = var.vmid
+
+  clone = "ubuntu2404"
+
+  cpu {
+    cores   = var.cores
+    sockets = var.sockets
+    type    = "host"
+  }
+
+  memory = var.memory
+  agent  = var.agent
+
+  network {
+    id     = 0
+    bridge = var.network_bridge
+    model  = "virtio"
+    mtu    = 1 # Inherit from bridge
+  }
+
+  os_type            = "ubuntu"
+  ipconfig0          = "ip=${var.ip_address}/24,gw=${var.gateway}"
+  start_at_node_boot = true
+
+  tags = "base,kubernetes_master,kubernetes_worker"
+
+  scsihw = "virtio-scsi-single"
+
+  disk {
+    type    = "cloudinit"
+    storage = "local-lvm"
+    slot    = "ide2"
+  }
+
+  disk {
+    size     = var.boot_disk_size
+    storage  = var.data_boot_storage
+    type     = "disk"
+    iothread = true
+    slot     = "virtio0"
+  }
+
+  disk {
+    size     = var.data_disk_size
+    storage  = var.data_disk_storage
+    type     = "disk"
+    iothread = true
+    slot     = "virtio1"
+  }
+
+}
--- terraform/hawkfield/main.tf
+++ terraform/hawkfield/main.tf
@@ -22,3 +22,15 @@ module "k8s-hawk-3" {
   vmid        = 113
   ip_address  = "10.1.2.103"
 }
+
+module "claw-hawk-1" {
+  source            = "../claw"
+  tags              = "base,claw"
+  name              = "claw-hawk-1"
+  target_node       = "hawk02"
+  vmid              = 122
+  ip_address        = "10.1.2.122"
+  cores             = 4
+  memory            = 8192
+  data_disk_storage = "local-lvm"
+}
