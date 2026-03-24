7371c15c648a66d774c6084f68f5a9661520d7dc
clincha <angus.clinch@gmail.com>
2026-03-24 21:18:56 +0000

    Set London data disk storage to local-lvm

--- terraform/london/main.tf
+++ terraform/london/main.tf
@@ -7,7 +7,8 @@ module "k8s-lon-1" {
   gateway        = "10.2.2.1"
   cores          = 4
   memory         = 8192
-  boot_disk_size = "50"
-  network_bridge = "vmbr0"
+  boot_disk_size    = "50"
+  data_disk_storage = "local-lvm"
+  network_bridge    = "vmbr0"
   tags           = "base,kubernetes_master,kubernetes_worker"
 }
