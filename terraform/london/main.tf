39bc45c1c4e0da25a798fa4b86484ddcf3e6edbd
clincha <angus.clinch@gmail.com>
2026-03-24 21:10:41 +0000

    Scale London to single VM and add London to Terraform apply workflow
    Reduce London from 3 identical k8s VMs to a single VM with 8GB RAM.
    Add London to the Terraform apply workflow using a matrix strategy.

--- terraform/london/main.tf
+++ terraform/london/main.tf
@@ -6,35 +6,7 @@ module "k8s-lon-1" {
   ip_address     = "10.2.2.101"
   gateway        = "10.2.2.1"
   cores          = 4
-  memory         = 4096
-  boot_disk_size = "50"
-  network_bridge = "vmbr0"
-  tags           = "base,kubernetes_master,kubernetes_worker"
-}
-
-module "k8s-lon-2" {
-  source         = "../modules/ubuntu-server"
-  name           = "k8s-lon-2"
-  target_node    = "lon01"
-  vmid           = 112
-  ip_address     = "10.2.2.102"
-  gateway        = "10.2.2.1"
-  cores          = 4
-  memory         = 4096
-  boot_disk_size = "50"
-  network_bridge = "vmbr0"
-  tags           = "base,kubernetes_master,kubernetes_worker"
-}
-
-module "k8s-lon-3" {
-  source         = "../modules/ubuntu-server"
-  name           = "k8s-lon-3"
-  target_node    = "lon01"
-  vmid           = 113
-  ip_address     = "10.2.2.103"
-  gateway        = "10.2.2.1"
-  cores          = 4
-  memory         = 4096
+  memory         = 8192
   boot_disk_size = "50"
   network_bridge = "vmbr0"
   tags           = "base,kubernetes_master,kubernetes_worker"
