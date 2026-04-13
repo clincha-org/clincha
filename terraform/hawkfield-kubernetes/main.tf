94c3ae58435987aeedbad24cdeb8719e22772b69
clincha <angus.clinch@gmail.com>
2026-04-13 06:45:03 +0100

    London network fix Kubernetes variables

--- terraform/hawkfield-kubernetes/main.tf
+++ terraform/hawkfield-kubernetes/main.tf
@@ -4,7 +4,7 @@ module "k8s-hawk-1" {
   target_node = "hawk01"
   vmid        = 111
   ip_address  = "10.1.2.101"
-  tags        = "base,kubernetes_master,kubernetes_worker"
+  tags        = "base,kubernetes,kubernetes_master,kubernetes_worker"
 }
 
 module "k8s-hawk-2" {
@@ -14,7 +14,7 @@ module "k8s-hawk-2" {
   vmid              = 112
   ip_address        = "10.1.2.102"
   data_disk_storage = "local-lvm"
-  tags              = "base,kubernetes_master,kubernetes_worker"
+  tags              = "base,kubernetes,kubernetes_master,kubernetes_worker"
 }
 
 module "k8s-hawk-3" {
@@ -23,5 +23,5 @@ module "k8s-hawk-3" {
   target_node = "hawk03"
   vmid        = 113
   ip_address  = "10.1.2.103"
-  tags        = "base,kubernetes_master,kubernetes_worker"
+  tags        = "base,kubernetes,kubernetes_master,kubernetes_worker"
 }
